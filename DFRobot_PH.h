452bb12c0a9924a7f792ec9afee4fe8891430ef3
cfd1 <57766372+cfd1@users.noreply.github.com>
2020-04-18 14:19:37 +0100

    Updated the DFRobot_PH.h
    Added a new constructor to allow the pin to be passed

--- DFRobot_PH.h
+++ DFRobot_PH.h
@@ -8,6 +8,10 @@
  *
  * version  V1.0
  * date  2018-04
+ * 
+ * version  V1.1
+ * date  2020-04
+ * Changes the memory addressing to allow multiple PH sensors
  */
 
 #ifndef _DFROBOT_PH_H_
@@ -24,12 +28,22 @@
 class DFRobot_PH
 {
 public:
+    // Construtors
     DFRobot_PH();
+    DFRobot_PH(int phPin);
+    
+    // Destructors
     ~DFRobot_PH();
-    void    calibration(float voltage, float temperature,char* cmd);  //calibration by Serial CMD
+    
+    // Initialization
+    void    begin();   
+    
+    // Calibration by Serial CMD
+    void    calibration(float voltage, float temperature, char* cmd);  
     void    calibration(float voltage, float temperature);
-    float   readPH(float voltage, float temperature); // voltage to pH value, with temperature compensation
-    void    begin();   //initialization
+    
+    // Voltage to pH value, with temperature compensation
+    float   readPH(float voltage, float temperature); 
 
 private:
     float  _phValue;
@@ -38,14 +52,18 @@ private:
     float  _voltage;
     float  _temperature;
 
-    char   _cmdReceivedBuffer[ReceivedBufferLength];  //store the Serial CMD
+    // Store the Serial CMD
+    char   _cmdReceivedBuffer[ReceivedBufferLength];  
     byte   _cmdReceivedBufferIndex;
 
 private:
+    // Calibration process, wirte key parameters to EEPROM
+    void    phCalibration(byte mode); 
+    
     boolean cmdSerialDataAvailable();
-    void    phCalibration(byte mode); // calibration process, wirte key parameters to EEPROM
-    byte    cmdParse(const char* cmd);
+        
     byte    cmdParse();
+    byte    cmdParse(const char* cmd);
 };
 
 #endif
